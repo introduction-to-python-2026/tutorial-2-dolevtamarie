912e6c6549a9df4e83bb861f0326cb27db9dd74d
dolevtamarie <dolevtamarie@gmail.com>
2025-10-30 11:49:01 +0200

    Implement find_max_number function

--- math_utils.py
+++ math_utils.py
@@ -1,2 +1,11 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+     if num1 > num2 :
+      if num1 > num3 :
+        return num1
+      else :
+        return num3
+    else :
+      if num2 > num3 :
+        return num2
+      else :
+        return num3

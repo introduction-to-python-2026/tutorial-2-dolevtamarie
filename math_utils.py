0e3e4e1e2462f5e7194b2ca83fa0c1f95bf02b4c
dolevtamarie <dolevtamarie@gmail.com>
2025-10-30 12:04:48 +0200

    Implement mean and standard deviation functions
    Add functions to calculate mean and standard deviation.

--- math_utils.py
+++ math_utils.py
@@ -9,3 +9,12 @@ def find_max_number(num1, num2, num3):
         return num2
       else :
         return num3
+
+def find_mean(num1, num2, num3):
+     mean = (num1 + num2 + num3) / 3
+     return mean
+
+def find_mean_std(num1, num2, num3):
+     mean = find_mean(num1, num2, num3)
+     sigma = (((num1-mean)**2 + (num2-mean)**2 + (num3-mean)**2/3)**0.5
+     return sigma
